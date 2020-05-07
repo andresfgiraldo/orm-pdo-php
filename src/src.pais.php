ba5029b4337b32e62acc8543cdc84bba917fbbe4
Andres Giraldo <andresfgiraldo@live.com>
2020-05-07 11:49:50 -0500

    Core Basic ORM with PDO for PHP

--- src/src.pais.php
+++ src/src.pais.php
@@ -2,78 +2,82 @@
 
 require_once(dirname(dirname(__FILE__)) . '/controller/ctr.pais.php');
 
+// PARA EFECTOS DE PRUEBAS, SE ASIGNA LA VARIABLE
+$_SESSION['app']['user']['username'] = 'andresfgiraldo';
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     extract($_GET, EXTR_PREFIX_ALL, "v");
 
-    if (strtolower($v_source) == "listar") {
-        header("Content-Type: application/json; charset=UTF-8");
-        http_response_code(200);
+    switch ($v_source) {
+        case "listar":
+            header("Content-Type: application/json; charset=UTF-8");
+            http_response_code(200);
 
-        $respuesta_pais = CtrPais::consultarTodos();
-        echo json_encode($respuesta_pais);
+            $respuesta_pais = CtrPais::consultarTodos();
+            echo json_encode($respuesta_pais);
 
-        return;
+            return;
+            break;
+
+        case "info":
+            header("Content-Type: application/json; charset=UTF-8");
+            http_response_code(200);
+
+            $respuesta_pais = CtrPais::consultar($v_id);
+            echo json_encode($respuesta_pais);
+
+            return;
+            break;
+
+        default:
+            /*
+            * Si no se llama algun metodo autorizado: 400 Bad Request
+            */
+            http_response_code(400);
+            return;   
     }
-
-    if (strtolower($v_source) == "info") {
-        header("Content-Type: application/json; charset=UTF-8");
-        http_response_code(200);
-
-        $respuesta_pais = CtrPais::consultar($v_id);
-        echo json_encode($respuesta_pais);
-
-        return;
-    }
-
-    /*
-     * Si no se llama algun metodo autorizado: 400 Bad Request
-     */
-    http_response_code(400);
-    return;
-}
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+}else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     extract($_POST, EXTR_PREFIX_ALL, "v");
 
-    if (strtolower($v_source) == "agregar") {
-        header("Content-Type: application/json; charset=UTF-8");
-        http_response_code(200);
+    switch ($v_source) {
+        case "agregar":
+            header("Content-Type: application/json; charset=UTF-8");
+            http_response_code(200);
 
-        $respuesta_pais = CtrPais::crear($v_alfa2,$v_alfa3, $v_numerico, $v_nombre);
-        echo json_encode($respuesta_ct);
+            $respuesta_pais = CtrPais::crear($v_alfa2, $v_alfa3, $v_numerico, $v_nombre);
+            echo json_encode($respuesta_pais);
 
-        return;
+            return;
+            break;
 
-    }
+        case "actualizar":
+            header("Content-Type: application/json; charset=UTF-8");
+            http_response_code(200);
 
-    if (strtolower($v_source) == "actualizar") {
-        header("Content-Type: application/json; charset=UTF-8");
-        http_response_code(200);
+            $respuesta_pais = CtrPais::actualizar($v_alfa2, $v_alfa3, $v_numerico, $v_nombre);
+            echo json_encode($respuesta_pais);
 
-        $respuesta_pais = CtrPais::actualizar($v_alfa2,$v_alfa3, $v_numerico, $v_nombre);
-        echo json_encode($respuesta_ct);
+            return;
+            break;
 
-        return;
+        case "eliminar":
+            header("Content-Type: application/json; charset=UTF-8");
+            http_response_code(200);
 
-    }
+            $respuesta_pais = CtrPais::eliminar($v_alfa2);
+            echo json_encode($respuesta_pais);
 
-    if (strtolower($v_source)  == "eliminar") {
-        header("Content-Type: application/json; charset=UTF-8");
-        http_response_code(200);
+            return;
+            break;
 
-        $respuesta_pais = CtrPais::eliminar($v_alfa2);
-        echo json_encode($respuesta_ct);
-
-        return;
-
-    }
-
-    /*
-     * Si no se llama algun metodo autorizado: 400 Bad Request
-     */
-    http_response_code(400);
-    return;
-
+        default:
+            /*
+            * Si no se llama algun metodo autorizado: 400 Bad Request
+            */
+            http_response_code(400);
+            return;   
+    }    
 }
